fadb0a437ee847425f59fecf40cf7266b76ad5bf
nathanrwatt-web <nathanrwatt@gmail.com>
2026-04-19 23:13:24 -0400

    added some list stuff, not yet fully implemented

--- crates/tui/src/main.rs
+++ crates/tui/src/main.rs
@@ -7,8 +7,10 @@ use ratatui::{
     DefaultTerminal, Frame,
 };
 
+
 struct App {
     todo_list: Vec<String>,
+    editor: Editor,
     should_quit: bool,
 }
 
@@ -73,4 +75,79 @@ fn main() -> Result<()> {
     let result = App::new().run(&mut terminal);
     ratatui::restore();
     result
+}
+
+struct Editor {
+    input: String, 
+    character_index : usize,
+    input_mode: InputMode
+} 
+
+enum InputMode {
+    Normal, 
+    Editing,
+}
+
+impl Editor {
+    fn new() -> Self {
+        Self {
+            input: String::new(),
+            character_index: 0,
+            input_mode: InputMode::Normal,
+        }
+    }
+
+    fn move_cursor_left(&mut self) {
+        let cursor_moved_left = self.character_index.saturating_sub(1);
+        self.character_index = self.clamp_cursor(cursor_moved_left);
+    }
+
+    fn move_cursor_right(&mut self) {
+        let cursor_moved_right = self.character_index.saturating_add(1);
+        self.character_index = self.clamp_cursor(cursor_moved_right);
+    }
+
+    fn enter_char(&mut self, new_char: char) {
+        let index = self.byte_index();
+        self.input.insert(index, new_char);
+        self.move_cursor_right();
+    }
+
+    fn byte_index(&self) -> usize {
+        self.input
+            .char_indices()
+            .map(|(i, _)| i)
+            .nth(self.character_index)
+            .unwrap_or(self.input.len())
+    }
+
+    fn delete_char(&mut self) {
+        let is_not_cursor_leftmost = self.character_index != 0;
+        if is_not_cursor_leftmost {
+
+            let current_index = self.character_index - 1;
+            let from_left_to_current_index = current_index - 1;
+
+            let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
+            let after_char_to_delete = self.input.chars().skip(current_index);
+
+            self.input = before_char_to_delete.chain(after_char_to_delete).collect();
+            self.move_cursor_left();
+        }
+    }
+
+
+    fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
+        new_cursor_pos.clamp(0, self.input.chars().count())
+    }
+
+    fn reset_cursor(&mut self) {
+        self.character_index = 0;
+    }
+
+    fn sumbit_list(&mut self, app: &mut App) {
+        app.todo_list.push(self.input.clone());
+        self.input.clear();
+        self.reset_cursor();
+    }
 }
